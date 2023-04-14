3b52770abc0bf0de5e2322b29d58f772024ed79b
Vicente Andres Vasquez Gonzalez <isaacgamerhd95@gmail.com>
2023-04-13 21:19:15 -0400

    Agregado de Formulario vanilla

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const nombre = document.getElementById('name');
+const apellido = document.getElementById('appaterno');
+const email = document.getElementById('email');
+const propuesta = document.getElementById('propuesta');
+const form = document.getElementById('form');
+const warning = document.getElementById('warning');
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let warningText = ""
+    let val = false
+    let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    if (nombre.value.length < 5){
+        warningText = `Nombre muy corto`
+        val = true
+    }
+    if (apellido.value.length < 5){
+        warningText = `Apellido muy corto`
+        val = true
+    }
+    if (!regex.test(email.value)){
+        warningText = `El email no es valido`
+        val = true
+    }
+
+    if (propuesta.value.length < 30){
+        warningText = `El porque es muy corto`
+        val = true
+    }
+    
+    if(val){
+        warning.innerHTML = warningText;
+    } else{
+        warning.innerHTML = ""
+    }
+    
+
+})
