6d51df17a51736cfd336a64f21ce46712e8991fb
Vicente Andres Vasquez Gonzalez <isaacgamerhd95@gmail.com>
2023-05-20 17:56:04 -0400

    Create Models
    Models User, Zapatilla, Categoria

--- StreetSneaks/userApp/models.py
+++ StreetSneaks/userApp/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Zapatilla(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Nombre")
-    precio = models.IntegerField(default=0, verbose_name="Valor")
-    talla = models.IntegerField(default=0, verbose_name="Talla")
-    disponible = models.BooleanField(default=False, verbose_name="Disponible")
-    tipo = models.CharField(max_length=15, verbose_name="Tipo")
-
-
 class Usuario(models.Model):
     name = models.CharField(max_length=15, verbose_name="Nombre")
     last_name = models.CharField(max_length=15, verbose_name="Apellido")
@@ -17,3 +9,4 @@ class Usuario(models.Model):
     class Meta:
         verbose_name = "Usuario"
         verbose_name_plural = "Usuarios"
+
